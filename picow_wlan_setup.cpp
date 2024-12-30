98f05452ae00b015d165dafbc2c48c4163b93630
Martin Abel <martin4abel@gmail.com>
2024-12-31 00:50:40 +0100

    update eeprom driver, fix some wlan stuff

--- picow_wlan_setup.cpp
+++ picow_wlan_setup.cpp
@@ -14,7 +14,6 @@ extern "C" {
 #include "m24c0x/src/m24c0x.hpp"
 
 #define TCP_PORT 80
-#define DEBUG_printf printf
 #define POLL_TIME_S 5
 #define HTTP_GET "GET"
 #define HTTP_POST "POST /pb-set-wifi"
@@ -47,7 +46,6 @@ typedef struct TCP_SERVER_T_ {
     struct tcp_pcb *server_pcb;
     bool complete;
     ip_addr_t gw;
-    async_context_t *context;
 } TCP_SERVER_T;
 
 typedef struct TCP_CONNECT_STATE_T_ {
@@ -68,18 +66,22 @@ static M24C0x eeprom(I2C_PORT, I2C_ADDR, I2C_SDA, I2C_SCL, WC_PIN);
 
 void save_wlan_config_to_eeprom()
 {
+    printf("save credentilas to eeprom");
     eeprom.write_bytes(0, (uint8_t*)wifi_ssid, WLAN_CRED_BUFFER_SIZE);
     eeprom.write_bytes(WLAN_CRED_BUFFER_SIZE, (uint8_t*)wifi_password, WLAN_CRED_BUFFER_SIZE);
     uint8_t savedFlag[1] = {WLAN_CRED_SAVED_FLAG};
     eeprom.write_bytes(2*WLAN_CRED_BUFFER_SIZE, savedFlag, 1);
+    printf("save credentilas to eeprom, done");
 }
 
 bool load_wlan_config_from_eeprom()
 {
+    printf("read credentilas from eeprom");
     eeprom.read_bytes(0, (uint8_t*)wifi_ssid, WLAN_CRED_BUFFER_SIZE);
     eeprom.read_bytes(WLAN_CRED_BUFFER_SIZE, (uint8_t*)wifi_password, WLAN_CRED_BUFFER_SIZE);
     uint8_t savedFlag[1] = {0};
     eeprom.read_bytes(2*WLAN_CRED_BUFFER_SIZE, savedFlag, 1);
+    printf("read credentilas from eeprom, done");
     if(savedFlag[0] == WLAN_CRED_SAVED_FLAG) {
         return true;
     }else{
@@ -135,7 +137,7 @@ static err_t tcp_close_client_connection(TCP_CONNECT_STATE_T *con_state, struct 
         tcp_err(client_pcb, NULL);
         err_t err = tcp_close(client_pcb);
         if (err != ERR_OK) {
-            DEBUG_printf("close failed %d, calling abort\n", err);
+            printf("close failed %d, calling abort\n", err);
             tcp_abort(client_pcb);
             close_err = ERR_ABRT;
         }
@@ -156,10 +158,10 @@ static void tcp_server_close(TCP_SERVER_T *state) {
 
 static err_t tcp_server_sent(void *arg, struct tcp_pcb *pcb, u16_t len) {
     TCP_CONNECT_STATE_T *con_state = (TCP_CONNECT_STATE_T*)arg;
-    DEBUG_printf("tcp_server_sent %u\n", len);
+    printf("tcp_server_sent %u\n", len);
     con_state->sent_len += len;
     if (con_state->sent_len >= con_state->header_len + con_state->result_len) {
-        DEBUG_printf("all done\n");
+        printf("all done\n");
         return tcp_close_client_connection(con_state, pcb, ERR_OK);
     }
     return ERR_OK;
@@ -202,15 +204,16 @@ static int wlan_config_server_content(const char *request, const char *params, c
     return len;
 }
 
+
 err_t tcp_server_recv(void *arg, struct tcp_pcb *pcb, struct pbuf *p, err_t err) {
     TCP_CONNECT_STATE_T *con_state = (TCP_CONNECT_STATE_T*)arg;
     if (!p) {
-        DEBUG_printf("connection closed\n");
+        printf("connection closed\n");
         return tcp_close_client_connection(con_state, pcb, ERR_OK);
     }
     assert(con_state && con_state->pcb == pcb);
     if (p->tot_len > 0) {
-        DEBUG_printf("tcp_server_recv %d err %d\n", p->tot_len, err);
+        printf("tcp_server_recv %d err %d\n", p->tot_len, err);
 
         bool send_response = false;
 
@@ -229,8 +232,8 @@ err_t tcp_server_recv(void *arg, struct tcp_pcb *pcb, struct pbuf *p, err_t err)
                 memcpy(payload, p_body, p_body_length);
                 payload[p_body_length] = 0;
 
-                DEBUG_printf("POST body ", p_body);
-                DEBUG_printf("POST body length ", p_body_length);
+                printf("POST body ", p_body);
+                printf("POST body length ", p_body_length);
 
                 // Scan the query string
                 sscanf(payload, "\r\n\r\nssid=%50[^&]&password=%50[^&]", wifi_ssid_tmp, wifi_pw_tmp);
@@ -245,14 +248,12 @@ err_t tcp_server_recv(void *arg, struct tcp_pcb *pcb, struct pbuf *p, err_t err)
                 url_decode(wifi_ssid, wifi_ssid_tmp);
                 url_decode(wifi_password, wifi_pw_tmp);
 
+
                 // gen result page
                 con_state->result_len = snprintf(con_state->result, sizeof(con_state->result), HTML_RESULT_BODY);
 
                 send_response = true;
                 wifi_is_saved = true;
-
-                // save to eeprom
-                save_wlan_config_to_eeprom();
             }
         }
 
@@ -283,7 +284,7 @@ err_t tcp_server_recv(void *arg, struct tcp_pcb *pcb, struct pbuf *p, err_t err)
         {
             // Check we had enough buffer space
             if (con_state->result_len > sizeof(con_state->result) - 1) {
-                DEBUG_printf("Too much result data %d\n", con_state->result_len);
+                printf("Too much result data %d\n", con_state->result_len);
                 return tcp_close_client_connection(con_state, pcb, ERR_CLSD);
             }
 
@@ -292,21 +293,21 @@ err_t tcp_server_recv(void *arg, struct tcp_pcb *pcb, struct pbuf *p, err_t err)
                 con_state->header_len = snprintf(con_state->headers, sizeof(con_state->headers), HTTP_RESPONSE_HEADERS,
                     200, con_state->result_len);
                 if (con_state->header_len > sizeof(con_state->headers) - 1) {
-                    DEBUG_printf("Too much header data %d\n", con_state->header_len);
+                    printf("Too much header data %d\n", con_state->header_len);
                     return tcp_close_client_connection(con_state, pcb, ERR_CLSD);
                 }
             } else {
                 // Send redirect
                 con_state->header_len = snprintf(con_state->headers, sizeof(con_state->headers), HTTP_RESPONSE_REDIRECT,
                     ipaddr_ntoa(con_state->gw));
-                DEBUG_printf("Sending redirect %s", con_state->headers);
+                printf("Sending redirect %s", con_state->headers);
             }
 
             // Send the headers to the client
             con_state->sent_len = 0;
             err_t err = tcp_write(pcb, con_state->headers, con_state->header_len, 0);
             if (err != ERR_OK) {
-                DEBUG_printf("failed to write header data %d\n", err);
+                printf("failed to write header data %d\n", err);
                 return tcp_close_client_connection(con_state, pcb, err);
             }
 
@@ -314,7 +315,7 @@ err_t tcp_server_recv(void *arg, struct tcp_pcb *pcb, struct pbuf *p, err_t err)
             if (con_state->result_len) {
                 err = tcp_write(pcb, con_state->result, con_state->result_len, 0);
                 if (err != ERR_OK) {
-                    DEBUG_printf("failed to write result data %d\n", err);
+                    printf("failed to write result data %d\n", err);
                     return tcp_close_client_connection(con_state, pcb, err);
                 }
             }
@@ -327,14 +328,14 @@ err_t tcp_server_recv(void *arg, struct tcp_pcb *pcb, struct pbuf *p, err_t err)
 
 static err_t tcp_server_poll(void *arg, struct tcp_pcb *pcb) {
     TCP_CONNECT_STATE_T *con_state = (TCP_CONNECT_STATE_T*)arg;
-    DEBUG_printf("tcp_server_poll_fn\n");
+    printf("tcp_server_poll_fn\n");
     return tcp_close_client_connection(con_state, pcb, ERR_OK); // Just disconnect clent?
 }
 
 static void tcp_server_err(void *arg, err_t err) {
     TCP_CONNECT_STATE_T *con_state = (TCP_CONNECT_STATE_T*)arg;
     if (err != ERR_ABRT) {
-        DEBUG_printf("tcp_client_err_fn %d\n", err);
+        printf("tcp_client_err_fn %d\n", err);
         tcp_close_client_connection(con_state, con_state->pcb, err);
     }
 }
@@ -342,15 +343,15 @@ static void tcp_server_err(void *arg, err_t err) {
 static err_t tcp_server_accept(void *arg, struct tcp_pcb *client_pcb, err_t err) {
     TCP_SERVER_T *state = (TCP_SERVER_T*)arg;
     if (err != ERR_OK || client_pcb == NULL) {
-        DEBUG_printf("failure in accept\n");
+        printf("failure in accept\n");
         return ERR_VAL;
     }
-    DEBUG_printf("client connected\n");
+    printf("client connected\n");
 
     // Create the state for the connection
     TCP_CONNECT_STATE_T *con_state = (TCP_CONNECT_STATE_T*)calloc(1, sizeof(TCP_CONNECT_STATE_T));
     if (!con_state) {
-        DEBUG_printf("failed to allocate connect state\n");
+        printf("failed to allocate connect state\n");
         return ERR_MEM;
     }
     con_state->pcb = client_pcb; // for checking
@@ -368,23 +369,23 @@ static err_t tcp_server_accept(void *arg, struct tcp_pcb *client_pcb, err_t err)
 
 static bool tcp_server_open(void *arg, const char *ap_name) {
     TCP_SERVER_T *state = (TCP_SERVER_T*)arg;
-    DEBUG_printf("starting server on port %d\n", TCP_PORT);
+    printf("starting server on port %d\n", TCP_PORT);
 
     struct tcp_pcb *pcb = tcp_new_ip_type(IPADDR_TYPE_ANY);
     if (!pcb) {
-        DEBUG_printf("failed to create pcb\n");
+        printf("failed to create pcb\n");
         return false;
     }
 
     err_t err = tcp_bind(pcb, IP_ANY_TYPE, TCP_PORT);
     if (err) {
-        DEBUG_printf("failed to bind to port %d\n",TCP_PORT);
+        printf("failed to bind to port %d\n",TCP_PORT);
         return false;
     }
 
     state->server_pcb = tcp_listen_with_backlog(pcb, 1);
     if (!state->server_pcb) {
-        DEBUG_printf("failed to listen\n");
+        printf("failed to listen\n");
         if (pcb) {
             tcp_close(pcb);
         }
@@ -398,27 +399,6 @@ static bool tcp_server_open(void *arg, const char *ap_name) {
     return true;
 }
 
-// This "worker" function is called to safely perform work when instructed by key_pressed_func
-void key_pressed_worker_func(async_context_t *context, async_when_pending_worker_t *worker) {
-    assert(worker->user_data);
-    printf("Disabling wifi\n");
-    cyw43_arch_disable_ap_mode();
-    ((TCP_SERVER_T*)(worker->user_data))->complete = true;
-}
-
-static async_when_pending_worker_t key_pressed_worker = {
-        .do_work = key_pressed_worker_func
-};
-
-void key_pressed_func(void *param) {
-    assert(param);
-    int key = getchar_timeout_us(0); // get any pending key press but don't wait
-    if (key == 'd' || key == 'D') {
-        // We are probably in irq context so call wifi in a "worker"
-        async_context_set_work_pending(((TCP_SERVER_T*)param)->context, &key_pressed_worker);
-    }
-}
-
 int main() {
     stdio_init_all();
 
@@ -426,20 +406,14 @@ int main() {
 
     TCP_SERVER_T *state = (TCP_SERVER_T*)calloc(1, sizeof(TCP_SERVER_T));
     if (!state) {
-        DEBUG_printf("failed to allocate state\n");
+        printf("failed to allocate state\n");
         return 1;
     }
 
     if (cyw43_arch_init()) {
-        DEBUG_printf("failed to initialise\n");
+        printf("failed to initialise\n");
         return 1;
     }
-
-    // Get notified if the user presses a key
-    state->context = cyw43_arch_async_context();
-    key_pressed_worker.user_data = state;
-    async_context_add_when_pending_worker(cyw43_arch_async_context(), &key_pressed_worker);
-    stdio_set_chars_available_callback(key_pressed_func, state);
 
     cyw43_arch_enable_ap_mode(WLAN_AP_SSID, WLAN_AP_PASSWORD, CYW43_AUTH_WPA2_AES_PSK);
 
@@ -456,7 +430,7 @@ int main() {
     dns_server_init(&dns_server, &state->gw);
 
     if (!tcp_server_open(state, WLAN_AP_SSID)) {
-        DEBUG_printf("failed to open server\n");
+        printf("failed to open server\n");
         return 1;
     }
 
@@ -496,6 +470,7 @@ int main() {
             }
             if (wifi_is_saved)
             {
+                printf("Try to connect to saved wlan settings");
                 wifi_is_saved = false;
 
                 // all led on, for waiting
@@ -509,6 +484,7 @@ int main() {
                 cyw43_arch_enable_sta_mode();
 
                 if (cyw43_arch_wifi_connect_timeout_ms(wifi_ssid, wifi_password, CYW43_AUTH_WPA2_AES_PSK, 10000)) {
+                    printf("connection to wifi not ok");
                     // connection to wifi not ok
                     gpio_put(LED_GREEN_GPIO, 0);
                     gpio_put(LED_RED_GPIO, 1);
@@ -520,9 +496,13 @@ int main() {
                 }
                 else
                 {
+                    printf("connection to wifi ok");
                     // connection to wifi ok
                     gpio_put(LED_GREEN_GPIO, 1);
                     gpio_put(LED_RED_GPIO, 0);
+
+                    // save to eeprom
+                    save_wlan_config_to_eeprom();
                 }
             }
             sleep_ms(50);
